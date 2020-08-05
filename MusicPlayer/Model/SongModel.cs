a4dcbd2e807facc11c0c5c4b882d05fd529bbf75
Dalish <Dalish@DESKTOP-S7S9PJT>
2020-08-05 12:38:23 +0200

    Added CRUD methods in SongModel class

--- MusicPlayer/Model/SongModel.cs
+++ MusicPlayer/Model/SongModel.cs
@@ -3,10 +3,92 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MusicPlayer.Model
 {
     class SongModel
     {
+        public List<Song> GetAllSongs()
+        {
+            try
+            {
+                using (AudioPlayerEntities context = new AudioPlayerEntities())
+                {
+                    return (from x in context.Songs select x).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+        }
+
+        public Song GetSong(int ID)
+        {
+            try
+            {
+                using (AudioPlayerEntities context = new AudioPlayerEntities())
+                {
+                    return (from x in context.Songs where x.SongID == ID select x).FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+        }
+
+        public void DeleteSong(int ID)
+        {
+            try
+            {
+                using (AudioPlayerEntities context = new AudioPlayerEntities())
+                {
+                    context.Songs.Remove((from x in context.Songs where x.SongID == ID select x).FirstOrDefault());
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void AddSong (Song song)
+        {
+            try
+            {
+                using (AudioPlayerEntities context = new AudioPlayerEntities())
+                {
+                    context.Songs.Add(song);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void UpdateSong (Song song)
+        {
+            try
+            {
+                using (AudioPlayerEntities context = new AudioPlayerEntities())
+                {
+                    Song s = (from x in context.Songs where x.SongID == song.SongID select x).FirstOrDefault();
+                    s = song;
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
     }
 }
